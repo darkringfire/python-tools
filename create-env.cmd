12926ed5b21bf76437548efb6fed3050d4fd27fa
A.Kosorotikov <darkringfire@gmail.com>
2023-05-31 08:59:09 +0800

    First commin
    + .gitignore
    + requirements
    + env create script

---
+++ create-env.cmd
@@ -0,0 +1,4 @@
+python -m venv --upgrade env
+.\env\Scripts\python.exe -m pip install -U pip
+.\env\Scripts\python.exe -m pip install -U setuptools
+.\env\Scripts\python.exe -m pip install -U -r requirements.txt
